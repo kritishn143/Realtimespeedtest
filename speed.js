0e6745e0bba9144b7f9c17e6773c86e085877b83
Kritishn143 <kritishneupane@gmail.com>
2024-01-02 21:13:50 +0545

    Initial commit

---
+++ speed.js
@@ -0,0 +1,25 @@
+const FastSpeedtest = require("fast-speedtest-api");
+
+// Replace "your-app-token" with your actual API token
+const speedtest = new FastSpeedtest({
+    token: "your_token_here",
+    verbose: false,
+    timeout: 10000,
+    https: true,
+    urlCount: 5,
+    bufferSize: 8,
+    unit: FastSpeedtest.UNITS.Mbps,
+    proxy: 'http://optional:auth@my-proxy:123'
+});
+
+// Perform download speed test
+speedtest.getSpeed().then(downloadSpeed => {
+    console.log(`Download Speed: ${downloadSpeed.toFixed(2)} Mbps`);
+
+    // Perform upload speed test
+    return speedtest.getSpeed('upload');
+}).then(uploadSpeed => {
+    console.log(`Upload Speed: ${uploadSpeed.toFixed(2)} Mbps`);
+}).catch(error => {
+    console.error(`Speed test failed: ${error.message}`);
+});
